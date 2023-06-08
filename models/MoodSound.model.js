c4428e2a248d26b3f9d816c89d878c1134e9f0ba
Jogurgithub412 <bitterspine@hotmail.com>
2023-06-08 23:38:44 +0100

    upate

--- models/MoodSound.model.js
+++ models/MoodSound.model.js
@@ -5,7 +5,10 @@ const moodSoundSchema = new Schema({
     type: String,
     enum: ['Happy','Excited','Optimistic','Serene','Relaxed','Bored', 'Apathetic','Lonely','Sad','Frustrated','Anxious','Irritated','Livid','Fed Up']
   },
-  soundUrl:[String]
+  sound:String ,
+  url:{
+    type: String
+  }
 });
 
 const MoodSound = model("MoodSound", moodSoundSchema);
---
+++ routes/mood.routes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+// Require the Noise Model 
+const Mood = require('../models/Mood.model');
+
+// GET Route home page after the login
+router.get("/home", async (req, res) => {
+  try{
+    let foundMoods = await Mood.find();
+    res.render('mood/moodtracker.jsx', {moods: foundMoods});
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+module.exports = router;
